62a248607eb146c97752ce5b95e9cbddb347ddad
Daniel Berezansky <dberezansky@outlook.com>
2022-11-28 17:32:41 -0800

    Implemented isopod worker

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <json.hpp>
 
 #include <iostream>
 #include <vector>
@@ -17,10 +16,9 @@
 #define TIMEOUT 100000  // 100 ms
 
 using namespace std;
-using nlohmann::json;
 
 // Requests batch of work from server
-// Args: 
+// Args:
 //  - sockfd: socket file descriptor
 //  - buf: the buffer to write into
 // Returns:
@@ -28,14 +26,14 @@ using nlohmann::json;
 string poll(int sockfd, char* buf);
 
 // Parses the response from the server
-// Args: 
+// Args:
 //  - response: the string response from the server
 // Returns:
 //  - vector<Message>: the parsed messages in a vector
 vector<Message> parseResponse(string response);
 
 // Parses the response from the server
-// Args: 
+// Args:
 //  - vector<Message> messages: the messages to process (print payload).
 // Returns:
 //  - bool: true on success, false otherwise.
@@ -43,10 +41,10 @@ bool process(vector<Message> messages);
 
 // Accepts server ip address as first arg
 int main(int argc, char** argv) {
-    if(argc != 2){
-        perror("Error: Incorrect number of arguments");
-        exit(EXIT_FAILURE);
-    }
+  if (argc != 2) {
+    perror("Error: Incorrect number of arguments");
+    exit(EXIT_FAILURE);
+  }
 
   int sockfd;
   char buf[MAXLINE];
@@ -57,18 +55,19 @@ int main(int argc, char** argv) {
     exit(EXIT_FAILURE);
   }
 
-    // Clear and set memory
-    bzero(&servaddr, sizeof(servaddr));
-    in_addr_t ip = inet_addr(argv[1]);
-    servaddr.sin_family = AF_INET6;
-    servaddr.sin_addr.s_addr = htonl(ip);
-    servaddr.sin_port = htons(PORT);
+  // Clear and set memory
+  bzero(&servaddr, sizeof(servaddr));
+  in_addr_t ip = inet_addr(argv[1]);
+  servaddr.sin_family = AF_INET6;
+  servaddr.sin_addr.s_addr = htonl(ip);
+  servaddr.sin_port = htons(PORT);
 
-    // connect to the server
-    if((connect(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)){
-        perror ("Error: Connection failed.");
-        exit(EXIT_FAILURE);
-    }
+  // connect to the server
+  if ((connect(sockfd, (const struct sockaddr*)&servaddr, sizeof(servaddr)) <
+       0)) {
+    perror("Error: Connection failed.");
+    exit(EXIT_FAILURE);
+  }
 
   // Event Loop: Poll and Process
   while (true) {
@@ -98,39 +97,34 @@ string poll(int sockfd, char* buf) {
   size_t find_position = buffer_.find('\0');
   while (find_position == string::npos) {
     int num_read;
-    if ((num_read = recv(sockfd, (char*) buf, MAXLINE, MSG_WAITALL)) < 0) {
-        perror("Timeout Error: Failure to Receive Messages.");
-        exit(EXIT_FAILURE);
+    if ((num_read = recv(sockfd, (char*)buf, MAXLINE, MSG_WAITALL)) < 0) {
+      perror("Timeout Error: Failure to Receive Messages.");
+      exit(EXIT_FAILURE);
     }
 
     if (num_read == 0) {
       break;
     } else if (num_read < 0) {
-        perror("Timeout Error: Failure to Receive Messages.");
-        exit(EXIT_FAILURE);
+      perror("Timeout Error: Failure to Receive Messages.");
+      exit(EXIT_FAILURE);
     }
-    buffer_ += std::string(reinterpret_cast<char*>(buf), num_read);
-    find_position =  buffer_.find('\0');
+    buffer_ += string(reinterpret_cast<char*>(buf), num_read);
+    find_position = buffer_.find('\0');
   }
 
   return buffer_;
 }
 
 vector<Message> parseResponse(string response) {
-  vector<Message> result;
-  
-   // "{}{}{}\0"
-   int close_bracket = response.find("}");
-
-   while(close_bracket != string::npos){
-        Message m = Message::deserialize(response.substr(0, close_bracket + 1));
-        result.push_back(m);
-        
-        response = response.substr(close_bracket + 1);
-        int close_bracket = response.find("}");
-   }
-
-  return result;
+  vector<Message> batch;
+  int front = 0;
+  for (int i = 0; i < response.size(); i++) {
+    if (response[i] = '}') {
+      batch.push_back(Message::deserialize(response.substr(front, i)));
+      front = i;
+    }
+  }
+  return batch;
 }
 
 bool process(vector<Message> messages) {
